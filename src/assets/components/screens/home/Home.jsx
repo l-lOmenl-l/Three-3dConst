cbd752f95c12781d4e8180fc60114659313d5da5
Artem <>
2023-11-01 17:45:44 +0300

    rebuild gui ver 1.0 (01.11.2023)

--- src/assets/components/screens/home/Home.jsx
+++ src/assets/components/screens/home/Home.jsx
@@ -5,10 +5,13 @@ import {params_closet} from './build/closet'
 
 
 
+
+
   
 
 
 export const Home = () => {
+
   function changeRender({ target }) {
     switch (true) {
       case target.name=='width':
@@ -32,28 +35,29 @@ export const Home = () => {
 
   return (
     <div className={styles.mainBody}>
+
       <div className={styles.lineMenu}>
         <p className="text-btn">Высота</p>
         <div className={styles.lineBtn}>
-          <input className="btn" name="height" onChange={changeRender} min="50" max="300" type="range" defaultValue="220" />
+          <input className={styles.inputRange} name="height" onChange={changeRender} min="50" max="300" type="range" defaultValue="220" />
         </div>
       </div>
       <div className={styles.lineMenu}>
         <p className="text-btn">Ширина</p>
         <div className={styles.lineBtn}>
-          <input className="btn" name="width" onChange={changeRender}  min="100" max="300" type="range" defaultValue="160" />
+          <input className={styles.inputRange} name="width" onChange={changeRender}  min="100" max="300" type="range" defaultValue="160" />
         </div>
       </div>
       <div className={styles.lineMenu}>
         <p className="text-btn">Глубина</p>
         <div className={styles.lineBtn}>
-          <input className="btn" name="depth" onChange={changeRender}  min="35" max="75" type="range" defaultValue="65" />
+          <input className={styles.inputRange} name="depth" onChange={changeRender}  min="35" max="75" type="range" defaultValue="65" />
         </div>
       </div>
       <div className={styles.lineMenu}>
         <p className="text-btn">Секции</p>
         <div className={styles.lineBtn}>
-          <input className="btn" name="section" onChange={changeRender}  min="1" max="9" type="range" defaultValue="2" />
+          <input className={styles.inputRange} name="section" onChange={changeRender}  min="2" max="9" type="range" defaultValue="2" />
         </div>
       </div>
         <BuildCloset />
